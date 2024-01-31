fe4cd40a8328bb66fb093d31ff86e42c93a05e03
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-31 12:21:47 +0100

    Hasierako pagina weba ta /shop behintzet kargatzeie honekin

--- website_sale_assign_agent_customer/models/__init__.py
+++ website_sale_assign_agent_customer/models/__init__.py
@@ -1,7 +1,9 @@
 ###############################################################################
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
-from . import sale_order
-from . import product_template
 from . import agent_partner
+# from . import coupon_program_groups This makes an error
 from . import coupon_program
+from . import product_template
+from . import sale_order
+from . import sale_order_line

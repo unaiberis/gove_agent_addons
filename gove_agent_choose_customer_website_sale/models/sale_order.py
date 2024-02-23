1074a5f18e35945e6d69585cd23190457ef9a38a
Unai Beristain <unaiberis@gmail.com>
2024-02-23 15:43:34 +0100

    Etxin ondo dabil

--- gove_agent_choose_customer_website_sale/models/sale_order.py
+++ gove_agent_choose_customer_website_sale/models/sale_order.py
@@ -14,13 +14,3 @@ class SaleOrder(models.Model):
                 order.purchase_finished = False
             else:
                 order.purchase_finished = True
-
-    # Add a method to force recomputation
-    def force_recompute_purchase_finished(self):
-        for order in self:
-            order.purchase_finished = order._compute_purchase_finished()
-
-# Usage example:
-# Call the force_recompute_purchase_finished method on the records you want to recompute
-orders_to_recompute = self.env['sale.order'].search([])
-orders_to_recompute.force_recompute_purchase_finished()

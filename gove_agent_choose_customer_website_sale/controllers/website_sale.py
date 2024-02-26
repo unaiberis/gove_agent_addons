f94836140fd583358da4162f346d3e4c00c75ad1
Unai Beristain <unaiberis@gmail.com>
2024-02-26 18:54:18 +0100

    [IMP] website_sale gove agent

--- gove_agent_choose_customer_website_sale/controllers/website_sale.py
+++ gove_agent_choose_customer_website_sale/controllers/website_sale.py
@@ -23,132 +23,139 @@ _logger = logging.getLogger(__name__)
 
 class WebsiteSale(WebsiteSale):
 
-    def _check_payment_confirmation(
-        self, order=None, create_mail_follower=False, **post
-    ):
-        _logger.info(
-                f"\n\n CHECK PAYMENT CONFIRMATION \n"
+    def _check_payment_confirmation(self, order=None, create_mail_follower=False, **post):
+        _logger.info("\n\n CHECK PAYMENT CONFIRMATION \n")
+
+        is_agent = request.env.user.partner_id.agent
+
+        if not is_agent:
+            order = self._get_last_order_for_customer(order)
+
+        elif is_agent:
+            order = self._get_last_order_for_agent(order)
+
+            if order and create_mail_follower:
+                self._handle_mail_follower_creation(order)
+
+        return order
+
+    def _get_last_order_for_customer(self, order=None):
+        last_order = (
+            request.env["sale.order"]
+            .sudo()
+            .search(
+                [
+                    ("partner_id", "=", request.env.user.partner_id.id),
+                    ("agent_customer", "=", False),
+                ],
+                order="id desc",
+                limit=1,
             )
-        if not request.env.user.partner_id.agent:
-            last_order = (
-                request.env["sale.order"]
-                .sudo()
-                .search(
-                    [
-                        ("partner_id", "=", request.env.user.partner_id.id),
-                        ("agent_customer", "=", False),
-                    ],
-                    order="id desc",
-                    limit=1,
-                )
+        )
+
+        self._update_order_info(order, last_order)
+
+        return order
+
+    def _get_last_order_for_agent(self, order=None):
+        agent_customer = int(
+            request.env["agent.partner"]
+            .sudo()
+            .search([("agent_id", "=", request.env.user.sudo().partner_id.id)], limit=1)
+            .customer_id_chosen_by_agent
+        )
+
+        partner_id = request.env.user.partner_id.id
+
+        last_order = (
+            request.env["sale.order"]
+            .sudo()
+            .search(
+                [
+                    ("partner_id", "=", partner_id),
+                ],
+                order="id desc",
+                limit=1,
             )
-            # Tipo = Pedido Surf
-            last_order.type_id = 2
-            
-            _logger.info(
-                f"\n\n Last order en _check_payment_confirmation {last_order} {request.env.user.partner_id} {last_order.name} User Name: {request.env.user.sudo().partner_id.name}\n"
+        )
+
+        last_order_customer = (
+            request.env["sale.order"]
+            .sudo()
+            .search(
+                [
+                    ("partner_id", "=", agent_customer),
+                ],
+                order="id desc",
+                limit=1,
             )
-            if not order or order and order.id < last_order.id:
-                order = last_order
+        )
 
-                request.session["sale_last_order_id"] = order.id
-
-                _logger.info(
-                    f"\n\n if not order siendo cliente _check_payment_confirmation {last_order} {last_order.name} {request.env.user.partner_id} User Name: {request.env.user.sudo().partner_id.name}\n"
-                )
-
-            else:
-                _logger.info(
-                    f"\n\n if not order else siendo cliente _check_payment_confirmation {order} {order.name} {request.env.user.partner_id} User Name: {request.env.user.sudo().partner_id.name}\n"
-                )
-
-        elif request.env.user.sudo().partner_id.agent:
-            _agent_customer = int(
+        if last_order and (not last_order_customer or last_order.id > last_order_customer.id):
+            order = self._update_order_info(order, last_order)
+            order.agent_customer = int(
                 request.env["agent.partner"]
                 .sudo()
-                .search(
-                    [("agent_id", "=", request.env.user.sudo().partner_id.id)], limit=1
-                )
+                .search([("agent_id", "=", request.env.user.sudo().partner_id.id)], limit=1)
                 .customer_id_chosen_by_agent
             )
+            request.session["sale_last_order_id"] = order.id
 
-            partner_id = request.env.user.partner_id.id
+            if create_mail_follower:
+                self._handle_mail_follower_creation(order)
 
-            # Find the last sale order for the given partner_id
-            last_order = (
-                request.env["sale.order"]
-                .sudo()
-                .search(
-                    [
-                        ("partner_id", "=", partner_id),
-                    ],
-                    order="id desc",  # leno "date_order desc" zeon
-                    limit=1,
-                )
+        elif last_order_customer:
+            last_order_customer.agent_customer = agent_customer
+
+            _logger.info(
+                f"\n\n last_order_customer {last_order_customer} {last_order_customer.name} User Name: {request.env.user.sudo().partner_id.name}\n"
             )
 
-            last_order_customer = (
-                request.env["sale.order"]
-                .sudo()
-                .search(
-                    [
-                        ("partner_id", "=", _agent_customer),
-                    ],
-                    order="id desc",  # leno "date_order desc" zeon
-                    limit=1,
-                )
+        return order
+
+    def _update_order_info(self, order, last_order):
+        _logger.info(
+            f"\n\n Last order in _check_payment_confirmation {last_order} {request.env.user.partner_id} {last_order.name} User Name: {request.env.user.sudo().partner_id.name}\n"
+        )
+
+        if not order or (order and order.id < last_order.id):
+            order = last_order
+            request.session["sale_last_order_id"] = order.id
+            _logger.info(
+                f"\n\n if not order being a customer _check_payment_confirmation {last_order} {last_order.name} {request.env.user.partner_id} User Name: {request.env.user.sudo().partner_id.name}\n"
             )
-            if last_order and (
-                not last_order_customer or last_order.id > last_order_customer.id
-            ):
-                order = last_order
-                _logger.info(
-                    f"\n\n AGENT LAST_ORDER (not last_order_customer or last_order.id > last_order_customer.id) {order} {order.name}  User Name: {request.env.user.sudo().partner_id.name}\n"
-                )
-                order.agent_customer = int(
-                    request.env["agent.partner"]
-                    .sudo()
-                    .search(
-                        [("agent_id", "=", request.env.user.sudo().partner_id.id)],
-                        limit=1,
-                    )
-                    .customer_id_chosen_by_agent
-                )
-                request.session["sale_last_order_id"] = order.id
-                _logger.info(
-                    f"\n\n SALE LAST ORDER ID request.session['sale_last_order_id'] = {request.session.get('sale_last_order_id')}, Order Name: {request.env['sale.order'].sudo().browse(request.session.get('sale_last_order_id')).name} User Name: {request.env.user.sudo().partner_id.name}\n"
-                )
+        else:
+            _logger.info(
+                f"\n\n if not order else being a customer _check_payment_confirmation {order} {order.name} {request.env.user.partner_id} User Name: {request.env.user.sudo().partner_id.name}\n"
+            )
 
+        return order
 
-                if create_mail_follower:
-                    order.partner_id = order.agent_customer.id
-                    _logger.info(f"\n\nONCHANGE antes del partner User Name: {request.env.user.sudo().partner_id.name}\n")
-                    order.onchange_partner_id()
-                    order.user_id = request.env.user.id
-                    # Tipo = Pedido Surf
-                    order.type_id = 2
-                    existing_follower = request.env["mail.followers"].search(
-                        [
-                            ("res_model", "=", "sale.order"),
-                            ("res_id", "=", order.id),
-                            ("partner_id", "=", order.agent_customer.id),
-                        ]
-                    )
+    def _handle_mail_follower_creation(self, order):
+        order.partner_id = order.agent_customer.id
+        _logger.info(f"\n\nONCHANGE before partner User Name: {request.env.user.sudo().partner_id.name}\n")
+        order.onchange_partner_id()
+        order.user_id = request.env.user.id
+        # Tipo = Pedido Surf
+        order.type_id = 2
 
-                    if not existing_follower:
-                        new_follower = request.env["mail.followers"].create(
-                            {
-                                "res_model": "sale.order",
-                                "res_id": order.id,
-                                "partner_id": order.agent_customer.id,
-                            }
-                        )
+        existing_follower = request.env["mail.followers"].search(
+            [
+                ("res_model", "=", "sale.order"),
+                ("res_id", "=", order.id),
+                ("partner_id", "=", order.agent_customer.id),
+            ]
+        )
 
-                        _logger.info(
-                            f"\n\n MAIL FOLLOWER Se ha creado asegurando que no existía: {new_follower} Comprador: {order.agent_customer.name} User Name: {request.env.user.sudo().partner_id.name}\n"
-                        )
+        if not existing_follower:
+            new_follower = request.env["mail.followers"].create(
+                {
+                    "res_model": "sale.order",
+                    "res_id": order.id,
+                    "partner_id": order.agent_customer.id,
+                }
+            )
 
-            elif last_order_customer:
-                last_order_customer.agent_customer = _agent_customer
-
-                _logger.info(f"\n\nlast_order_customer {last_order_customer} {last_order_customer.name} User Name: {request.env.user.sudo().partner_id.name}\n")
+            _logger.info(
+                f"\n\n MAIL FOLLOWER Created, ensuring it didn't exist: {new_follower} Buyer: {order.agent_customer.name} User Name: {request.env.user.sudo().partner_id.name}\n"
+            )

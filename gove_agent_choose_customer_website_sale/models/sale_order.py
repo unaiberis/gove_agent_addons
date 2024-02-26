2d996760158973a94d3e8eca94c10a80a59ea7fd
Unai Beristain <unaiberistain@avanzosc.es>
2024-02-26 10:23:54 +0100

    [IMP] _canceled_completed_orders_from_woo

--- gove_agent_choose_customer_website_sale/models/sale_order.py
+++ gove_agent_choose_customer_website_sale/models/sale_order.py
@@ -14,3 +14,14 @@ class SaleOrder(models.Model):
                 order.purchase_finished = False
             else:
                 order.purchase_finished = True
+                
+
+    @api.depends('woo_status')
+    def _canceled_completed_orders_from_woo(self):
+        for order in self:
+            if order.woo_status:
+                domain = ['|', ('state', 'not in', ['draft', 'sent', 'sale']), ('id', '=', False)]
+                negated_domain = ['!', domain]
+                
+                orders_to_cancel = order.search(negated_domain)
+                orders_to_cancel._action_cancel()

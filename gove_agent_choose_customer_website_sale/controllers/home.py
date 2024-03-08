eade4d9ead8a5ddf97a58777f70409f9a8672d40
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-08 12:50:42 +0100

    [IMP] trey_agent_modules

--- gove_agent_choose_customer_website_sale/controllers/home.py
+++ gove_agent_choose_customer_website_sale/controllers/home.py
@@ -1,5 +1,6 @@
 from odoo.addons.portal.controllers.web import Home
 
+# When someone logs in it redirects to / instead of to /my
 class Home(Home):
     def _login_redirect(self, uid, redirect=None):
         return super()._login_redirect(uid, redirect="/")

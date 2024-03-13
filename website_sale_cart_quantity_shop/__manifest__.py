b8372949d3eb374e3671267e37dd067c52b47d01
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-13 10:43:17 +0100

    [IMP] website sale cart quantity shop

--- website_sale_cart_quantity_shop/__manifest__.py
+++ website_sale_cart_quantity_shop/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Website Sale Cart Quantity Shop",
-    "summary": "Choose cart quantity from shop page"
+    "summary": "Choose cart quantity from shop page",
     "category": "Website",
     "version": "14.0.1.1.0",
     "author": "AvanzOSC",
--- website_sale_cart_quantity_shop/controllers/__init__.py
+++ website_sale_cart_quantity_shop/controllers/__init__.py
@@ -1,6 +1,5 @@
 ###############################################################################
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
-from . import home
 from . import website_sale
-# from . import website
+# from . impot website

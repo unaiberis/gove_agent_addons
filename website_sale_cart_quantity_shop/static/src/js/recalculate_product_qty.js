0c9c404f6481be2edb8b434a61cf6e800c7adcb8
Unai Beristain <unaiberis@gmail.com>
2024-03-15 19:20:58 +0100

    [IMP] It works perfectly

--- website_sale_cart_quantity_shop/static/src/js/recalculate_product_qty.js
+++ website_sale_cart_quantity_shop/static/src/js/recalculate_product_qty.js
@@ -1,3 +1,6 @@
+// Copyright 2024 Unai Beristain - AvanzOSC
+// License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 odoo.define("website_sale_cart_quantity_shop.recalculate_product_qty", function (require) {
     "use strict";
 

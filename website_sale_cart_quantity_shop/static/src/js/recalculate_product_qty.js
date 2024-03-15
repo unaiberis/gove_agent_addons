64017b7ed58076f30f469a734c10badb08d29626
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-15 12:26:54 +0100

    [IMP] Improve js buttons

--- website_sale_cart_quantity_shop/static/src/js/recalculate_product_qty.js
+++ website_sale_cart_quantity_shop/static/src/js/recalculate_product_qty.js
@@ -16,12 +16,12 @@ odoo.define("website_sale_cart_quantity_shop.recalculate_product_qty", function 
     publicWidget.registry.WebsiteSale.include({
         custom_add_qty: 0, // Definir custom_add_qty como propiedad de WebsiteSale
         changeTriggeredByButton: false, // Bandera para indicar si el cambio fue provocado por los botones
-        
+
         start: function () {
             this._super.apply(this, arguments);
-    
+
             var self = this;
-    
+
             $(".fa.fa-plus").parent().click(function (event) {
                 // Evitar el comportamiento predeterminado del enlace
                 event.preventDefault();
@@ -32,17 +32,17 @@ odoo.define("website_sale_cart_quantity_shop.recalculate_product_qty", function 
                 inputField.data('oldValue', newValue);
                 self.custom_add_qty = 1; // Asignar 1 para el botón de más
                 self.changeTriggeredByButton = true; // Establecer la bandera como verdadera
-                
+
                 console.log("Plus Button Clicked:");
                 console.log("Old Value:", oldValue);
                 console.log("New Value:", newValue);
                 console.log("Custom Add Qty:", self.custom_add_qty);
                 console.log("Change Triggered By Button:", self.changeTriggeredByButton);
-                
+
                 // Llamar a _onClickAdd con el botón como argumento
                 self._onClickAdd(event);
             });
-            
+
             // Modificar el evento click del botón .fa.fa-minus
             $(".fa.fa-minus").parent().click(function (event) {
                 // Evitar el comportamiento predeterminado del enlace
@@ -54,47 +54,47 @@ odoo.define("website_sale_cart_quantity_shop.recalculate_product_qty", function 
                 inputField.data('oldValue', newValue);
                 self.custom_add_qty = -1; // Asignar -1 para el botón de menos
                 self.changeTriggeredByButton = true; // Establecer la bandera como verdadera
-                
+
                 console.log("Minus Button Clicked:");
                 console.log("Old Value:", oldValue);
                 console.log("New Value:", newValue);
                 console.log("Custom Add Qty:", self.custom_add_qty);
                 console.log("Change Triggered By Button:", self.changeTriggeredByButton);
-                
+
                 // Llamar a _onClickAdd con el botón como argumento
                 self._onClickAdd(event);
             });
-            
+
 
             $("input.form-control.quantity").change(function (event) {
                 if (!self.changeTriggeredByButton) { // Verificar si el cambio no fue provocado por los botones
                     var oldValue = $(this).data('oldValue') || 0;
                     var newValue = parseInt($(this).val().replace(',', '.')) || 0;
-            
+
                     if (newValue < 0 || isNaN(newValue)) {
                         newValue = 0;
                     }
-            
+
                     $(this).val(newValue);
-            
+
                     console.log("Old Value: " + oldValue);
                     console.log("New Value: " + newValue);
-            
+
                     $(this).data('oldValue', newValue);
-            
+
                     self.custom_add_qty = newValue - oldValue; // Calcular la cantidad personalizada
-            
+
                     console.log("Custom Add Qty: " + self.custom_add_qty);
-            
+
                     // Llamar a _onClickAdd con el botón como argumento
                     self._onClickAdd(event);
                 } else {
                     self.changeTriggeredByButton = false; // Restablecer la bandera a falso para futuros cambios
                 }
             });
-            
+
         },
-    
+
         /**
          * @private
          * @param {MouseEvent} ev
@@ -105,7 +105,7 @@ odoo.define("website_sale_cart_quantity_shop.recalculate_product_qty", function 
             this.targetEl = $(ev.currentTarget);
             return this._super.apply(this, arguments);
         },
-    
+
         /**
          * Add custom variant values and attribute values that do not generate variants
          * in the form data and trigger submit.
@@ -117,34 +117,62 @@ odoo.define("website_sale_cart_quantity_shop.recalculate_product_qty", function 
             const pageType = this.pageType;
             const params = this.rootProduct;
             params.add_qty = this.custom_add_qty; // Utilizar this.custom_add_qty
-    
+
             console.log("Params: " + JSON.stringify(params));
-    
+
             params.product_custom_attribute_values = JSON.stringify(
                 params.product_custom_attribute_values
             );
             params.no_variant_attribute_values = JSON.stringify(
                 params.no_variant_attribute_values
             );
-    
+
             if (this.isBuyNow) {
+                console.log("Buy now is true.");
                 params.express = true;
             }
+            
             this._rpc({
-                route: "/shop/cart/ajaxify_update_json",
+                route: "/shop/cart/update_json_from_shop",
                 params: params,
             }).then((data) => {
+                console.log("Data received from RPC call:", data);
+            
+                // Integrate the logic for input value restriction here
+                $("input.form-control.quantity").each(function () {
+                    var $inputField = $(this);
+                    var oldValue = parseInt($inputField.val().replace(',', '.')) || 0;
+                    var currentQuantity = parseInt(data.quantity) || 0;
+            
+                    var newValue = Math.min(oldValue, currentQuantity);
+                    $inputField.val(newValue);
+            
+                    console.log("Input Field:", $inputField);
+                    console.log("Old Value:", oldValue);
+                    console.log("Current Quantity:", currentQuantity);
+                    console.log("New Value:", newValue);
+                });
+            
                 wSaleUtils.updateCartNavBar(data);
                 const $navButton = $("header .o_wsale_my_cart").parent();
                 let el = $();
                 if (pageType === "product") {
+                    console.log("Page type is product.");
                     el = $("#o-carousel-product");
                 }
                 if (pageType === "products") {
+                    console.log("Page type is products.");
                     el = this.targetEl.parents(".o_wsale_product_grid_wrapper");
                 }
+                console.log("Element selected:", el);
                 wSaleUtils.animateClone($navButton, el, 25, 40);
+            }).catch((error) => {
+                console.error("Error occurred during RPC call:", error);
             });
+
+
+
+
         },
     });
 });

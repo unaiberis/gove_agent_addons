f80b0f66815d92d69c1c2c6fd62253cf5685b909
Unai Beristain <unaiberis@gmail.com>
2024-05-13 09:57:27 +0200

    [IMP] Many2many

--- gove_agent_choose_customer_website_sale/models/product_public_category.py
+++ gove_agent_choose_customer_website_sale/models/product_public_category.py
@@ -3,7 +3,8 @@ from odoo import models, fields
 class ProductPublicCategory(models.Model):
     _inherit = 'product.public.category'
 
-    pricelist_ids = fields.One2many(
+    pricelist_ids = fields.Many2many(
         'product.pricelist', 
+        'category_id',
         string='Pricelists'
     )

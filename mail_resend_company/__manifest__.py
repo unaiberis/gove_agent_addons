aa567d20c36e5f5510c0aeff078ddbabd0d22d34
Unai Beristain <unaiberis@gmail.com>
2024-05-27 10:50:59 +0200

    [IMP] Separate module mail_resend_company

--- mail_resend_company/__manifest__.py
+++ mail_resend_company/__manifest__.py
@@ -1,9 +1,9 @@
 {
-    'name': 'Company Resend Email',
+    'name': 'Company Resend Mail',
     'version': '1.0',
     'category': 'Custom',
-    'summary': 'Adds a resend_email field to the company model',
-    'description': 'This module adds a unique resend_email field to each company in Odoo.',
+    'summary': 'Adds a resend_mail field to the company model and resends the mail to the company',
+    'description': 'This module adds a unique resend_mail field to each company in Odoo.',
     'author': 'Unai Beristain - AvanzOSC',
     'depends': ['base', 'mail'],
     'data': [

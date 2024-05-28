c6983057f43c2630ca1fcd5fb3b51dcb13fa9528
Unai Beristain <unaiberis@gmail.com>
2024-05-28 11:34:47 +0200

    [IMP] Add loggers

--- base_partner_sequence_odoobot/models/partner.py
+++ base_partner_sequence_odoobot/models/partner.py
@@ -1,12 +1,17 @@
 # Copyright 2024 Unai Beristain - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import models
 
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     def _needs_ref(self, vals=None):
+        _logger.info(f"\n\n _logger_gove NEEDS REF {vals}\n")
         if self.customer_from_woo:
+            _logger.info(f"\n\n _logger_gove CUSTOMER FROM WOOCOMMERCE {self}\n")
             return True
         return super()._needs_ref(vals)
